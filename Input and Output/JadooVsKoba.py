ae7c38d6886a3768859b9ae5953838ece52627e7
Anobhama V <anobhama99@gmail.com>
2020-08-21 02:34:54 +0530

    21/08/2020

---
+++ Input and Output/JadooVsKoba.py
@@ -0,0 +1,18 @@
+"""
+Jadoo, the space alien has challenged Koba to a friendly duel. He asks Koba to write a program to print out all numbers from 70 to 80. Knowing perfectly well how easy the problem is, the Jadoo adds his own twist to the challenge. He asks Koba to write the program without using a single number in his program and also restricts the character limit to 100
+
+Ouput:
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+"""
+for number in "FGHIJKLMNOP":
+    print(ord(number))
